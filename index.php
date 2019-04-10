98b7fd1690dfb56614c414e0cb3e949068385f7f
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-04-10 08:50:13 +0800

    Added UI for index

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<?php
+<?php require 'include/header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>JKR Sistem Aduan</h1>
+  <p>Selamat datang ke sistem 
+      aduan pelanggan JKR!</p> 
+</div>
+  
+<div class="container">
+  <div class="row">
+    
+  </div>
+</div>
+
+</body>
+</html>
